b660532044133d69d0910d4fafee59361bed7f27
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-08-23 19:00:24 -0300

    feat: install typescript config, create components folder and modify App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+import Title from "./components/Title";
+
+export default function App() {
+  return (
+    <View>
+      <Title/>
+    </View>
+  )
+}
---
+++ src/components/Title.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native"
+
+const Title = () => {
+  return (
+    <Text>Hello world!</Text>
+  )
+}
+
+export default Title
