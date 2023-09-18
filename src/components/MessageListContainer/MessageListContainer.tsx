0bd9026c9efb8980e7cda2e97dad3404b544daf9
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-09-18 16:03:12 -0300

    feat: add drawer in MessageListContainer

--- src/components/MessageListContainer/MessageListContainer.tsx
+++ src/components/MessageListContainer/MessageListContainer.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { FlatList, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Button, Text } from 'react-native-paper';
 
 import MessageForm from '../MessageForm/MessageForm';
 import Message from '../Message/Message';
@@ -8,27 +8,40 @@ import Message from '../Message/Message';
 import type { IMessage } from '../../types';
 
 import styles from './MessageListContainer.styles';
+import { Drawer } from 'react-native-drawer-layout';
 
 const MessageListContainer = () => {
   const [messagesList, setMessagesList] = useState<IMessage[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.listContainer}>
-        {messagesList.length > 0 ? (
-          <FlatList
-            data={messagesList}
-            renderItem={({ item }) => <Message data={item} setMessagesList={setMessagesList} />}
-            keyExtractor={item => item.id}
-          />
-        ) : (
-          <Text variant="headlineMedium" style={styles.textAlert}>
-            No hay mensajes
-          </Text>
-        )}
+    <Drawer
+      open={isOpen}
+      onOpen={() => setIsOpen(true)}
+      onClose={() => setIsOpen(false)}
+      drawerPosition="right"
+      renderDrawerContent={() => {
+        return <Text>Drawer content</Text>;
+      }}
+    >
+      <View style={styles.container}>
+        <View style={styles.listContainer}>
+          {messagesList.length > 0 ? (
+            <FlatList
+              data={messagesList}
+              renderItem={({ item }) => <Message data={item} setMessagesList={setMessagesList} />}
+              keyExtractor={item => item.id}
+            />
+          ) : (
+            <Text variant="headlineMedium" style={styles.textAlert}>
+              No hay mensajes
+            </Text>
+          )}
+        </View>
+        {}
+        <MessageForm setMessagesList={setMessagesList} />
       </View>
-      <MessageForm setMessagesList={setMessagesList} />
-    </View>
+    </Drawer>
   );
 };
 
