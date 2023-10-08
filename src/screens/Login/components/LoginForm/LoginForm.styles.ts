7030ee122f30ab5f84058230bfd87293c406517f
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-10-08 12:32:56 -0300

    style: modify structure of LoginForm component, and add navigation to SignupForm

--- src/screens/Login/components/LoginForm/LoginForm.styles.ts
+++ src/screens/Login/components/LoginForm/LoginForm.styles.ts
@@ -1,19 +1,44 @@
 import { StyleSheet } from 'react-native';
 
+import colors from '../../../../constants/colors';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  formContainer: {
+    flex: 1,
     justifyContent: 'center',
-    gap: 30
+    gap: 25
+  },
+  inputsContainer: {
+    padding: 20,
+    gap: 25
+  },
+  redirectContainer: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    padding: 15,
+    gap: 10
+  },
+  redirectText: {
+    textAlign: 'center',
+    fontSize: 15
+  },
+  redirectButton: {
+    color: colors.primary
   },
   button: {
     backgroundColor: 'black',
-    padding: 20,
+    padding: 5,
+
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 30
+    gap: 30,
+    margin: 20,
+    borderRadius: 15
   }
 });
 
--- src/screens/Login/components/LoginForm/LoginForm.tsx
+++ src/screens/Login/components/LoginForm/LoginForm.tsx
@@ -1,24 +1,46 @@
+import { useNavigation } from '@react-navigation/native';
+
 import { Formik } from 'formik';
-import { View } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
+import { View, ScrollView, TouchableOpacity } from 'react-native';
+import { TextInput, Button, Text } from 'react-native-paper';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import styles from './LoginForm.styles';
+
+import type { StackNavigation } from '../../../../navigation/types';
 
 interface LoginFormProps {
   handleSubmit: (values: { email: string; password: string }) => Promise<void>;
 }
 
 const LoginForm = ({ handleSubmit }: LoginFormProps) => {
+  const { navigate } = useNavigation<StackNavigation>();
+
   return (
     <Formik initialValues={{ email: '', password: '' }} onSubmit={values => handleSubmit(values)}>
       {({ handleBlur, handleChange, handleSubmit: submit, values }) => (
-        <View style={styles.container}>
-          <TextInput onChangeText={handleChange('email')} onBlur={handleBlur('email')} value={values.email}></TextInput>
-          <TextInput onChangeText={handleChange('password')} onBlur={handleBlur('password')} value={values.password}></TextInput>
-          <Button textColor="white" contentStyle={styles.button} onPress={() => submit()}>
-            Enviar
-          </Button>
-        </View>
+        <KeyboardAwareScrollView contentContainerStyle={styles.container}>
+          <ScrollView contentContainerStyle={styles.formContainer}>
+            <View style={styles.inputsContainer}>
+              <TextInput label="Email" onChangeText={handleChange('email')} onBlur={handleBlur('email')} value={values.email}></TextInput>
+              <TextInput
+                label="Contraseña"
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+              ></TextInput>
+            </View>
+            <View style={styles.redirectContainer}>
+              <Text style={styles.redirectText}>¿No tienes una cuenta?</Text>
+              <TouchableOpacity onPress={() => navigate('Signup')}>
+                <Text style={styles.redirectButton}>Regístrate</Text>
+              </TouchableOpacity>
+            </View>
+            <Button textColor="white" contentStyle={styles.button} onPress={() => submit()}>
+              Iniciar sesión
+            </Button>
+          </ScrollView>
+        </KeyboardAwareScrollView>
       )}
     </Formik>
   );
